c42a9f3f16f95db4a39d19676f9aded36de21720
codeInn001 <emmaalege@gmail.com>
2022-11-09 12:28:00 +0100

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import SignUp from './routes/SignUp'
+import Login from './routes/SignUp'
 import './index.css'
 import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
 
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignUp/>
+  },
+  {
+    path: "/login",
+    element: <Login/>
   }
 ])
 
